e339030f41a6818dba8036513d776762f1850ad5
troulungble <troulungble@gmail.com>
2018-10-16 01:52:42 +0800

    //

--- app/js/app.js
+++ app/js/app.js
@@ -18,3 +18,26 @@ angular.module('myApp').config(function(ezfbProvider) {
 angular.module('myApp').constant('constantVariable', {
     PAGE_SIZE : 15
 });
+
+angular.module('myApp').filter('range', function() {
+    return function(input, total) {
+        total = parseInt(total);
+
+        for (var i=0; i<total; i++) {
+            input.push(i);
+        }
+
+        return input;
+    };
+});
+// angular.module('myApp').run(function($rootScope) {
+//     $rootScope.range = function(min, max, step) {
+//         step = step || 1;
+//         var input = [];
+//         for (var i = min; i <= max; i += step) {
+//             input.push(i);
+//         }
+//         console.info(input) ;
+//         return input;
+//     };
+// });
